5935a9017617bef69d6bb420590fb8c9a382a82a
arman4hossain <95018506+arman4hossain@users.noreply.github.com>
2024-12-28 22:58:01 +0600

    jobs-details-server-added

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ async function run() {
         });
 
         
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id ; 
+            const query = {_id: id}
+            const result = await bookCollection.findOne(query) ; 
+            res.send(result) ; 
+        })
+        
         app.patch('/jobs/:id', async (req, res) => {
             console.log("Request received:", req.body); // Debugging
             const id = req.params.id;
